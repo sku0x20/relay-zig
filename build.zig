916a6bf45998e3cc8633388aab1d554a56e3b6aa
siddhant <siddhantkumarupmanyu@gmail.com>
2026-01-05 02:09:26 +0530

    update build script

--- build.zig
+++ build.zig
@@ -1,3 +1,17 @@
 const std = @import("std");
 
-pub fn build(_: *std.Build) void {}
+pub fn build(b: *std.Build) void {
+    const exe = b.addExecutable(.{
+        .name = "relay",
+        .root_module = b.createModule(.{
+            .root_source_file = b.path("src/main.zig"),
+            .target = b.graph.host,
+            .optimize = .Debug,
+        }),
+    });
+    b.installArtifact(exe);
+
+    const run_artifact = b.addRunArtifact(exe);
+    const run_step = b.step("run", "Run the project");
+    run_step.dependOn(&run_artifact.step);
+}
