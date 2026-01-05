d8c7874e7d3bef1be858c24584e44a0a022a9d52
siddhant <siddhantkumarupmanyu@gmail.com>
2026-01-06 01:32:26 +0530

    Refactor build script to modularize unit test creation

--- build.zig
+++ build.zig
@@ -1,4 +1,5 @@
 const std = @import("std");
+const Walker = @import("std").fs.Dir.Walker;
 
 pub fn build(b: *std.Build) void {
     const exe = b.addExecutable(.{
@@ -27,27 +28,34 @@ fn unit_tests(b: *std.Build) void {
     defer walker.deinit();
 
     while (walker.next() catch null) |entry| {
-        // rename files to .test.zig
-
-        if (entry.kind == .file and std.mem.endsWith(u8, entry.path, ".test.zig")) {
-            const path = b.pathJoin(&.{ "test", entry.path });
-            const unit_test = b.addTest(.{
-                .name = entry.basename,
-                .root_module = b.createModule(.{
-                    .root_source_file = b.path(path),
-                    .target = b.graph.host,
-                    .optimize = .Debug,
-                }),
-            });
-
-            const run_unit_test = b.addRunArtifact(unit_test);
-            test_step.dependOn(&run_unit_test.step);
-
-            // enable to add individual command also
-            // const test_name = entry.path;
-            // const test_run_step_name = std.fmt.allocPrint(b.allocator, "{s}{s}", .{ "test ", test_name }) catch "unknown";
-            // const ut_run_step = b.step(test_run_step_name, "");
-            // ut_run_step.dependOn(&run_unit_test.step);
-        }
+        create_individual_ut(b, test_step, entry);
     }
+}
+
+fn create_individual_ut(
+    b: *std.Build,
+    test_step: *std.Build.Step,
+    entry: Walker.Entry,
+) void {
+    if (entry.kind != .file and !std.mem.endsWith(u8, entry.path, ".test.zig")) {
+        return;
+    }
+    const path = b.pathJoin(&.{ "test", entry.path });
+    const unit_test = b.addTest(.{
+        .name = entry.basename,
+        .root_module = b.createModule(.{
+            .root_source_file = b.path(path),
+            .target = b.graph.host,
+            .optimize = .Debug,
+        }),
+    });
+
+    const run_unit_test = b.addRunArtifact(unit_test);
+    test_step.dependOn(&run_unit_test.step);
+
+    // enable to add individual command also
+    // const test_name = entry.path;
+    // const test_run_step_name = std.fmt.allocPrint(b.allocator, "{s}{s}", .{ "test ", test_name }) catch "unknown";
+    // const ut_run_step = b.step(test_run_step_name, "");
+    // ut_run_step.dependOn(&run_unit_test.step);
 }
